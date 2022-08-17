c5e60c238c370c966d29b1b1c9128cb72a382fc4
aaaaqqqqwwww <supernova45@naver.com>
2022-08-17 14:41:10 +0900

    final

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const cors = require("cors");
-
-app.set("port", 8099);
+const NAVER_ID = process.env.NAVER_ID;
+const NAVER_SECRET_ID = process.env.NAVER_SECRET_ID;
+app.set("port", process.env.PORT || 8099);
+const port = app.get("port");
 app.use(cors());
 
 //라우팅 (특정 주소로 사용자가 들어오면 해야하는 것)
@@ -16,8 +18,8 @@ app.get("/book/:bookname", (req, res) => {
   axios({
     url: `https://openapi.naver.com/v1/search/book.json?query=${queryTxt}`,
     headers: {
-      "X-Naver-Client-Id": "jSRsLa_WrpguF0mZMFFQ",
-      "X-Naver-Client-Secret": "42A8NqtW3a",
+      "X-Naver-Client-Id": NAVER_ID,
+      "X-Naver-Client-Secret": NAVER_SECRET_ID,
     },
   }).then(function (response) {
     // console.log(response.data);
@@ -33,6 +35,6 @@ app.get("/login", (req, res) => {
     res.send({ isLogged: false });
   }
 });
-app.listen(8099, function () {
-  console.log("8099에서 서버 대기 중");
+app.listen(port, function () {
+  console.log(`${port}에서 서버 대기 중`);
 });
